72c27cc36963b009cd68e5cad75a059ff27c136e
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-10-14 21:17:44 +0530

    Working on the product range on the main page

--- src/App.js
+++ src/App.js
@@ -11,21 +11,20 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path = '/' element ={<Shop/>}></Route>
-        <Route path = '/men' element ={<ShopCategory category = "men"/>}></Route>
-        <Route path = '/women' element ={<ShopCategory category = "women"/>}></Route>
-        <Route path = '/kids' element ={<ShopCategory category = "kids"/>}></Route>
-        <Route path  = '/product' element = {< Product/>}>
-          <Route path =":productId" element = {< Product/>}></Route>
-        </Route>
-        <Route path = '/cart' element ={<Cart/>}></Route>
-        <Route path = '/login' element ={<LoginSignUp/>}></Route>
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory category="men" />} />
+          <Route path="/women" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
