74419fbfc90ce03b3d36581b782877341f3e7523
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-10-17 18:51:28 +0530

    Create the popular section of the main page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/hero/Hero'
 import Popular from '../components/popular/Popular'
+import Offers from '../components/Offers/Offers'
 
 const Shop = () => {
   return (
     <div>
         <Hero></Hero>
         <Popular></Popular>
+        <Offers></Offers>
     </div>
   )
 }
