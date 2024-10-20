dc35a00cb766d4a082f55e9eb4c198e53d350932
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-10-20 18:08:26 +0530

    Create the shop category page for all the categories

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import LoginSignUp from './pages/LoginSignUp.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import men_banner from './components/assets/Frontend_Assets/banner_mens.png'
 import women_banner from './components/assets/Frontend_Assets/banner_women.png'
-import kids_banner from './components/assets/Frontend_Assets/banner_kids.png'
+import kid_banner from './components/assets/Frontend_Assets/banner_kids.png'
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" element={<Shop />} />
           <Route path="/men" element={<ShopCategory banner ={men_banner} category="men" />} />
           <Route path="/women" element={<ShopCategory banner = {women_banner} category="women" />} />
-          <Route path="/kids" element={<ShopCategory banner  = {kids_banner} category="kids" />} />
+          <Route path="/kids" element={<ShopCategory banner  = {kid_banner} category="kid" />} />
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignUp />} />
