8a4a7535ed35215c5f8100ccc7e287491187db24
Anirudh-Sama <anirudhsingh.as61@gmail.com>
2024-10-23 08:31:05 +0530

    Create the login page but it has an error in the footer

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignUp = () => {
   return (
-    <div></div>
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign-Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name"/>
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name ="" id="" />
+          <p>By agreeing, I accept the terms of use and privacy policies</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
